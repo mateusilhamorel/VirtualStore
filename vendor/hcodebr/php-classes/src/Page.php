b13857fd62197a9f153d6a9dad76dc37f48b9f89
Mateus <mateusilhamorel@gmail.com>
2020-06-10 14:12:32 -0300

    Esqueci a senha

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -19,7 +19,7 @@ class Page
         $this->options = array_merge($this->defaults, $opts);
         // config
         $config = array(
-    	      "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+    	    "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false // set to false to improve the speed
     				 );
