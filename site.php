afc6c1e3437efdde138c87481aad5c7235171e14
Mateus <mateusilhamorel@gmail.com>
2020-06-16 21:41:12 -0300

    Carrinho

--- site.php
+++ site.php
@@ -3,7 +3,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
-
+use \Hcode\Model\Cart;
 
 $app->get('/', function() {
 
@@ -46,5 +46,11 @@ $app->get("/products/:desurl", function($desurl){
     ]);
 });
 
+
+$app->get("/cart", function(){
+    $cart = Cart::getFromSession();
+    $page = new Page();
+    $page->setTpl("cart");
+});
 
 ?>
